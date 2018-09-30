124624098df11f14dea76425fa52fe50991ee42b
congyh <yihao.cong@outlook.com>
2018-09-30 16:17:27 +0800

    fix 由于可读取字节<int长度导致的魔数判断失败问题

--- src/main/java/the/flash/codec/Spliter.java
+++ src/main/java/the/flash/codec/Spliter.java
@@ -8,6 +8,7 @@ import the.flash.protocol.PacketCodeC;
 public class Spliter extends LengthFieldBasedFrameDecoder {
     private static final int LENGTH_FIELD_OFFSET = 7;
     private static final int LENGTH_FIELD_LENGTH = 4;
+    private int count = 0;
 
     public Spliter() {
         super(Integer.MAX_VALUE, LENGTH_FIELD_OFFSET, LENGTH_FIELD_LENGTH);
@@ -15,7 +16,14 @@ public class Spliter extends LengthFieldBasedFrameDecoder {
 
     @Override
     protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
-        if (in.getInt(in.readerIndex()) != PacketCodeC.MAGIC_NUMBER) {
+        // 注意: tcp传输的一个物理帧的的大小是512bytes, 如果发生了拆包,
+        // 那么直接调用in.getInt(in.readerIndex())就很有可能出现数组越界的情况
+        // 测试用例: 客户端发送"test1"
+        // 服务端可以查到此输出: in.readerIndex(): 510, in.readableBytes(): 2
+        if (in.readableBytes() < 4) {
+            System.out.println("in.readerIndex(): " + in.readerIndex() +  ", in.readableBytes(): " + in.readableBytes());
+        }
+        if (in.readableBytes() >= 4 && in.getInt(in.readerIndex()) != PacketCodeC.MAGIC_NUMBER) {
             ctx.channel().close();
             return null;
         }
--- src/main/java/the/flash/server/handler/MessageRequestHandler.java
+++ src/main/java/the/flash/server/handler/MessageRequestHandler.java
@@ -5,13 +5,11 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import the.flash.protocol.request.MessageRequestPacket;
 import the.flash.protocol.response.MessageResponsePacket;
 
-import java.util.Date;
-
 public class MessageRequestHandler extends SimpleChannelInboundHandler<MessageRequestPacket> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, MessageRequestPacket messageRequestPacket) {
         MessageResponsePacket messageResponsePacket = new MessageResponsePacket();
-        System.out.println(new Date() + ": 收到客户端消息: " + messageRequestPacket.getMessage());
+//        System.out.println(new Date() + ": 收到客户端消息: " + messageRequestPacket.getMessage());
         messageResponsePacket.setMessage("服务端回复【" + messageRequestPacket.getMessage() + "】");
 
         ctx.channel().writeAndFlush(messageResponsePacket);
